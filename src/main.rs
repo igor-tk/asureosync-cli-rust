779175813b138fbd59128d5c3727971969b83da2
igor <tk.igor@gmail.com>
2020-11-07 15:19:39 +0300

    test 02

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, App
 use log::{debug, info, trace, warn, error};
 
 fn main() {
-    println!("Hello, world!");
+    println!("test 02");
 }
 
